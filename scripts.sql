ca4fa2ba71e3c98656891f4f817c6a6d1956a4f8
adam tsuchiyama <adamtsuchiyama@adams-MacBook-Pro.local>
2020-01-18 13:16:28 -0600

    First commit message!

--- scripts.sql
+++ scripts.sql
@@ -1,78 +1,78 @@
 /* Question 1 */
-/*SELECT COUNT(*)
+SELECT COUNT(*)
 FROM data_analyst_jobs;
-*/
+
 
 /* Question 2 */
-/*SELECT *
+SELECT *
 FROM data_analyst_jobs
 LIMIT 10;
-*/
+
 
 /* Question 3 */
-/*SELECT COUNT(*)
+SELECT COUNT(*)
 FROM data_analyst_jobs
 WHERE location = 'TN'
 OR location = 'KY';
-*/
+
 
 /* Question 4 */
-/*SELECT COUNT(*)
+SELECT COUNT(*)
 FROM data_analyst_jobs
 WHERE star_rating > 4;
-*/
+
 
 /* Question 5 */
-/*SELECT COUNT(*)
+SELECT COUNT(*)
 FROM data_analyst_jobs
 WHERE review_count > 500
 AND review_count < 1000;
-*/
+
 
 /* Question 6 */
-/*SELECT location AS state, AVG(star_rating) AS avg_rating
+SELECT location AS state, AVG(star_rating) AS avg_rating
 FROM data_analyst_jobs
 GROUP BY state
 ORDER BY avg_rating desc;
-*/
+
 
 /* Question 7 */
-/*SELECT COUNT(DISTINCT title)
+SELECT COUNT(DISTINCT title)
 FROM data_analyst_jobs;
-*/
+
 
 /* Question 8 */
-/*SELECT COUNT(DISTINCT title)
+SELECT COUNT(DISTINCT title)
 FROM data_analyst_jobs
 WHERE location = 'CA';
-*/
+
 
 /* Question 9 */
-/*SELECT company, AVG(star_rating) AS avg_rating
+SELECT company, AVG(star_rating) AS avg_rating
 FROM data_analyst_jobs
 WHERE review_count > 5000
 GROUP BY company;
-*/
+
 
 /* Question 10 */
-/*SELECT company, AVG(star_rating) AS avg_rating
+SELECT company, AVG(star_rating) AS avg_rating
 FROM data_analyst_jobs
 WHERE review_count > 5000
 GROUP BY company
 ORDER BY avg_rating desc;
-*/
+
 
 /* Question 11 */
-/*SELECT COUNT(DISTINCT title)
+SELECT COUNT(DISTINCT title)
 FROM data_analyst_jobs
 WHERE title LIKE '%nalyst%';
-*/
+
 
 /* Question 12 */
-/*SELECT DISTINCT title
+SELECT DISTINCT title
 FROM data_analyst_jobs
 WHERE title NOT LIKE '%nalyst%'
 AND title NOT LIKE '%ANALYST%'
 AND title NOT LIKE '%nalytics%'
 AND title NOT LIKE '%ANALYTICS%';
-*/
+
